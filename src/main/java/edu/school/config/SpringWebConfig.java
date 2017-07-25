38366a9a0c00c7901ad34f1b53220db4cada7dec
talong <513536236@qq.com>
2017-07-25 12:33:12 +0800

    删掉无用的import

--- src/main/java/edu/school/config/SpringWebConfig.java
+++ src/main/java/edu/school/config/SpringWebConfig.java
@@ -3,7 +3,6 @@ package edu.school.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
