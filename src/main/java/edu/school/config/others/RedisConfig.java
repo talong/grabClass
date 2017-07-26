d3114322be08b98a6c8ce5164fb962d8bdbc1020
talong <513536236@qq.com>
2017-07-26 23:45:01 +0800

    增加RedisConfig

---
+++ src/main/java/edu/school/config/others/RedisConfig.java
@@ -0,0 +1,45 @@
+package edu.school.config.others;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories("edu.school.dao")//使用Redis Repositories原因：Working with Redis Repositories allows
+//to seamlessly convert and store domain objects in Redis Hashes, apply custom mapping strategies and make 
+//use of secondary indexes.
+public class RedisConfig {
+
+	/**
+	 * 配置redis
+	 * @return
+	 */
+	@Bean
+	public RedisConnectionFactory connectionFactory() {
+		JedisConnectionFactory cf = new JedisConnectionFactory();
+		cf.setHostName("localhost");
+		cf.setPort(6379);
+		return cf;
+	}
+	
+/*	@Bean
+	public RedisTemplate<String, StuRefCourse> redisTemplate(RedisConnectionFactory cf) {
+		RedisTemplate<String, StuRefCourse> redis = new RedisTemplate<String, StuRefCourse>();
+		redis.setConnectionFactory(cf);
+		return redis;
+	}*/
+	
+	@Bean
+	public RedisTemplate<?, ?> redisTemplate() {
+
+	    RedisTemplate<byte[], byte[]> template = new RedisTemplate<byte[], byte[]>();
+	    template.setConnectionFactory(connectionFactory());
+	    return template;
+	}
+
+	
+	
+}
